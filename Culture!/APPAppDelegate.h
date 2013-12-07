07a2930710a24f4f2b48e32d2d583529cb3cde70
Tim Leytens <leytens.tim@gmail.com>
2013-12-07 14:25:13 +0100

    initial project commit

---
+++ Culture!/APPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APPAppDelegate.h
+//  Culture!
+//
+//  Created by Tim Leytens on 7/12/13.
+//  Copyright (c) 2013 Tim Leytens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
